65af8428cc9a5d299d91bfd8d2b7fcc1d48c33c3
txuko22 <brunoalvarezdarriba@gmail.com>
2024-05-30 21:29:08 +0200

    feat/Algoritmo finalizado para el juego del quien es quien

--- src/main.py
+++ src/main.py
@@ -9,15 +9,12 @@ prolog.consult("C:/Users/Bruno/Desktop/IA_BD/MIA/Quien_es_quien/src/bbdd.pl")
 personajes = []
 personajes_restantes = []
 todas_las_caracteristicas = []
-caracteristicas = set()
 
-# Obtenemos una lista con todos los nombres de los personajes del tablero con los que vamos a jugar (suponemos que las características ya se saben).
-def tablero():
-    resultado_query = list(prolog.query("personaje(Nombre,_)"))
-    for resultado in resultado_query:
-        personajes.append(resultado["Nombre"])
+# Imprimimos la lista de personajes que se nos pasa por parámetro (nombre y características).
+def tablero(lista_personajes):
+    for personaje in lista_personajes:
+        print(f"{personaje["Nombre"]}: {personaje["Caracteristicas"]}")
 
-    return personajes
 
 # Esta función nos devuelve un personaje aleatorio del tablero (con sus respectivas caracteristicas), el cuál va a ser el que deberemos adivinar.
 def eleccion_personaje():
@@ -26,49 +23,67 @@ def eleccion_personaje():
 
     return personaje
 
+# Devuelve la característica a preguntar siguiendo nuestro algoritmo (explicado en el README.md).
+def caracteristica(lista):
+    contador_caracteristicas = 0
+    todas_las_caracteristicas = []
 
-# Obtenemos una lista con todas la características distintas que aparecen en las diversas listas de características de cada uno de los personajes.
-def lista_caracteristicas():
-    resultado_query = list(prolog.query("personaje(_, Caracteristicas)"))
-    for resultado in resultado_query:
-        for c in resultado["Caracteristicas"]:
-            caracteristicas.add(c)
-
-    return caracteristicas
-
-# def pregunta():
-#     lista_caracteristicas = lista_caracteristicas()
-
-#     resultado_query = list(prolog.query("personaje(_, Caracteristicas)"))
-#     for resultado in resultado_query:
-#         todas_las_caracteristicas.extend(resultado["Caracteristicas"])
+    for p in lista:
+        for c in p["Caracteristicas"]:
+            todas_las_caracteristicas.append(c)
     
-#     contador_caracteristicas = Counter(todas_las_caracteristicas)
-#     caracteristica_mas_usada = contador_caracteristicas.most_common(1)[0][0] # Devuelve una lista don una tupla de [Característica, Número de veces que aparece], de ahí que solo acceda a la posición de la caracterítica
-#     print(caracteristica_mas_usada)
+    contador_caracteristicas = Counter(todas_las_caracteristicas)
+    contador_caracteristicas = sorted(contador_caracteristicas.items(), key=lambda i: i[1], reverse=True)
 
-#     consulta = f"personaje(Nombre, Caracteristicas), member({caracteristica}, Caracteristicas)"
-#     for resultado_query in prolog.query(consulta):
-#         personajes_restantes.append(resultado_query["Nombre"])
-    
-#     return personajes_restantes
+    return contador_caracteristicas[int(len(contador_caracteristicas)/2)][0]
 
+# Devuelve un booleano sobre si el personaje que se tiene que adivinar tiene la caracteristica que se le pasa o no.
+def pregunta(personaje_secreto, caract):
+    if caract in personaje_secreto["Caracteristicas"]:
+        return True
+    return False
 
-# result = list(prolog.query("personaje(herman, X)"))
-
-#for solution in result:
-#    print("Hombre:", solution["X"])
 
 def main():
-    personajes = tablero()
-    print("Personajes del tablero: ")
-    print(personajes)
+    preguntas = 0
 
+    # Obtenemos el personaje a adivinar.
     personaje_a_adivinar = eleccion_personaje()
-    print(personaje_a_adivinar)
+    print(f"El personaje a adivinar es: {personaje_a_adivinar["Nombre"]}({personaje_a_adivinar["Caracteristicas"]})")
 
-    
-    #for p in personajes:
-    #    print(f"\t{p}")
+    # Realizamos una consulta prolog para sacar todos los personajes del tablero según iniciamos la partida y los imprimimos.
+    resultado_query = list(prolog.query("personaje(Nombre,Caracteristicas)"))
+    print("\n\n<----------------------------------------------------->")
+    print("Personajes del tablero: ")
+
+    # Repetimos el bucle hasta que solo quede un único personaje.
+    while(len(resultado_query) != 1):
+        
+        tablero(resultado_query)
+        personajes_temp = []
+
+        # Sacamos la característica que tenemos que preguntar.
+        caracteristica_a_preguntar = caracteristica(resultado_query)
+        print(f"\nTu personaje tiene {caracteristica_a_preguntar}?")
+
+
+        # Usamos una nueva lista para añadir los personajes que si o no tengan la característica que se pregunta.
+        if pregunta(personaje_a_adivinar, caracteristica_a_preguntar):
+            print(f"Si, tiene {caracteristica_a_preguntar}\n")
+            for p in resultado_query:
+                if caracteristica_a_preguntar in p['Caracteristicas']:
+                    personajes_temp.append(p)
+        else:
+            print(f"No, no tiene {caracteristica_a_preguntar}\n")
+            for p in resultado_query:
+                if caracteristica_a_preguntar not in p['Caracteristicas']:
+                    personajes_temp.append(p)
+
+        # Imprimos la nueva lista resultante después de realizar la pregunta
+        resultado_query = personajes_temp
+        preguntas += 1
+
+    print(f"El personaje es: {resultado_query}")
+    print(f"Número de preguntas: {preguntas}")
 
 main()
